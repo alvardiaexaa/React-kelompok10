1e4cdeffd266bfa4555475f5c50c6ff76e049015
abdiel <abdielrayman@gmail.com>
2026-03-30 01:42:55 +0700

    progress sebelum pull

--- src/App.js
+++ src/App.js
@@ -1,44 +1,31 @@
-exa-profil
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ExaProfil from './ExaProfil';
-
-import logo from './logo.svg';
 import './App.css';
 import AbdielProfil from './abdielprofil';
+import ExaProfil from './ExaProfil';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
- master
 
 function App() {
   return (
     <Router>
-exa-profil
       <Routes>
-        {/* Akses halaman ini di localhost:3000/exa-profil */}
-        <Route path="/exa-profil" element={<ExaProfil />} />
-        
-        {/* Halaman Utama */}
-        <Route path="/" element={
-          <div className="h-screen flex flex-col items-center justify-center">
-            <h1 className="text-3xl font-bold text-indigo-600">Project Kelompok 10</h1>
-            <p className="mt-2 text-gray-500 font-medium">Ketik <span className="text-red-500">/exa-profil</span> pada alamat URL browser Anda</p>
-          </div>
-        } />
 
-      <Routes> 
-        <Route path="/abdielprofil" element={<AbdielProfil />} />
+        {/* HOME */}
         <Route path="/" element={
           <div className="App">
             <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
+              <h1>Project Kelompok 10 🚀</h1>
+              <p>Pilih halaman:</p>
+              <ul>
+                <li>/abdiel-profil</li>
+                <li>/exa-profil</li>
+              </ul>
             </header>
           </div>
         } />
 
+        {/* PROFIL */}
         <Route path="/abdiel-profil" element={<AbdielProfil />} />
-master
+        <Route path="/exa-profil" element={<ExaProfil />} />
+
       </Routes>
     </Router>
   );
