65eaea44bdcd672ab9e7bba9d7c1efd4e0fe2678
abdiel <abdielrayman@gmail.com>
2026-03-29 19:12:15 +0700

    Menambahkan profil abdiel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import AbdielProfil from './abdielprofil';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes> 
+        <Route path="/abdielprofil" element={<AbdielProfil />} />
+        <Route path="/" element={
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+            </header>
+          </div>
+        } />
+
+        <Route path="/abdiel-profil" element={<AbdielProfil />} />
+      </Routes>
+    </Router>
   );
 }
 
